a503f62a6fe4e59b576459c6eff1814f3fc9d84b
Justis Lamanna <lucbuifurqui@yahoo.com>
2016-12-12 16:12:35 -0600

    Added Auto_Increment

--- Type.sql
+++ Type.sql
@@ -1,29 +1,29 @@
 CREATE TABLE TYPE_Data
 (
-	Type_ID int NOT NULL PRIMARY KEY,
+	Type_ID int PRIMARY KEY AUTO_INCREMENT,
 	Type_Name varchar(10) NOT NULL
 );
 
 INSERT INTO TYPE_Data
-	(Type_ID, Type_Name)
+	(Type_Name)
 VALUES
-	(0, 'Normal'),
-	(1, 'Fighting'),
-	(2, 'Flying'),
-	(3, 'Poison'),
-	(4, 'Ground'),
-	(5, 'Rock'),
-	(6, 'Bug'),
-	(7, 'Ghost'),
-	(8, 'Steel'),
-	(9, 'Fire'),
-	(10, 'Water'),
-	(11, 'Grass'),
-	(12, 'Electric'),
-	(13, 'Psychic'),
-	(14, 'Ice'),
-	(15, 'Dragon'),
-	(16, 'Dark'),
-	(17, 'Fairy');
+	('Normal'),
+	('Fighting'),
+	('Flying'),
+	('Poison'),
+	('Ground'),
+	('Rock'),
+	('Bug'),
+	('Ghost'),
+	('Steel'),
+	('Fire'),
+	('Water'),
+	('Grass'),
+	('Electric'),
+	('Psychic'),
+	('Ice'),
+	('Dragon'),
+	('Dark'),
+	('Fairy');
 	
 	
